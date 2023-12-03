301b2221451289c742919be3207edeb0cc362dfd
Poulpi41 <thomann.raphael41@gmail.com>
2023-12-03 18:58:49 +0100

    fetch policy updated

--- synthListSite/sw.js
+++ synthListSite/sw.js
@@ -478,3 +478,18 @@ self.addEventListener("install", (event) => {
         addResourcesToCache(cacheAssets),
     );
 });
+async function cacheThenNetwork(request) {
+    const cachedResponse = await caches.match(request);
+    if (cachedResponse) {
+        console.log("Found response in cache:", cachedResponse);
+        return cachedResponse;
+    }
+    console.log("Falling back to network");
+    return fetch(request);
+}
+  
+self.addEventListener("fetch", (event) => {
+    console.log(`Handling fetch event for ${event.request.url}`);
+    event.respondWith(cacheThenNetwork(event.request));
+});
+  
